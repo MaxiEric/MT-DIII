29f5067607b665ccc2a4e76abef135a4e1459fa3
MaxiEric <maxiericgonzalez@gmail.com>
2018-10-22 16:48:07 -0300

    py

--- MT.py
+++ MT.py
@@ -9,16 +9,16 @@ def crearMaquina():
     estados = eval(input("Ingrese lista de estados (ej:['q1','FIN'] => "))
     # a = 'FIN' in estados
     crearFuncion(estados)
-       
+
 
 def crearFuncion ( q ):
     alfa = ['0','1']   
     n_filas = len(q)-1 #le resto el estado FIN
     m_col = 3
-    print (n_filas)   
+    print (n_filas)
     a = [[0] * m_col for i in range(n_filas)]
-    print (a)   
-    
+    print (a)
+
     for i in range (0,n_filas):
         # e = q[i]
         a[i][0]= q[i]
@@ -26,10 +26,10 @@ def crearFuncion ( q ):
         a[i][1] = e
         b = input("ingrese transicion 1")
         a[i][2] = b
-    
+
     print(tabulate(a,headers=['0', '1']))
 
- 
+
 # Menos intuitiva pero mas eficiente
 # matriz = [None] * numero_filas
 # for i in range(numero_filas):
@@ -40,10 +40,3 @@ crearMaquina()
 
 
 #implementacion de matrices en python : https://snakify.org/es/lessons/two_dimensional_lists_arrays/
-
-
-
-
-
-
-
